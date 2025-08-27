034864d78ed77dd1e4ba0c13d19aab794c7c3e5b
Tristine <tristineferriera@gmail.com>
2025-08-26 21:21:43 -0600

    Fixing the LFS problem

--- UnityProject/Assets/ArtisanDream.Tools-master/SingleVariables/FloatAxis.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(menuName = "Single Variables/Float Axis")]
-public class FloatAxis : FloatData
-{
-    [SerializeField] private string axis = "Horizontal";
-}
--- UnityProject/Assets/ArtisanDream.Tools-master/SingleVariables/SimpleBoolData.cs
+++
@@ -1,8 +0,0 @@
-
-using UnityEngine;
-[CreateAssetMenu(menuName = "Single Variables/SimpleBoolData")]
-// This class is a simplified version of BoolData, without events and setters.
-public class SimpleBoolData : ScriptableObject
-{
-   public bool value;
-}
